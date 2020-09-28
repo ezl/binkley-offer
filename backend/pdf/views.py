1d8a98f260c682ddf39bbf203264a1389e171c92
Frankie Primerano <frankieprimerano@gmail.com>
2020-09-28 11:42:34 -0400

    Django Port - PdfViewSet working

--- backend/pdf/views.py
+++ backend/pdf/views.py
@@ -1,19 +1,55 @@
-from rest_framework.viewsets import ViewSet
-from rest_framework.exceptions import ParseError
+import os
 
-from .serializers import PdfSerializer
+from types import SimpleNamespace
+
+from django.http import FileResponse
+
+from rest_framework.viewsets import ViewSet
+from rest_framework.exceptions import ParseError, NotFound
+from rest_framework.response import Response
+
+from .serializers import CreatePdfSerializer, GetPdfSerializer
+from .models import Pdf
 
 class PdfViewSet(ViewSet):
-    serializer_class = PdfSerializer 
-    
+    serializer_class = CreatePdfSerializer
+
     def create(self, request):
-        serializer = PdfSerializer(data=request.data)
+        serializer = CreatePdfSerializer(data=request.data)
         
         if not serializer.is_valid():
             raise ParseError(detail=serializer.errors)
 
-    def retrieve(self, request):
-        pass
+        pdf = serializer.save()
+        pdf_src = pdf.pdf_src
+
+        if not os.path.exists(pdf_src):
+            raise NotFound(detaul="PDF not found")
+
+        return FileResponse(open(pdf_src, 'rb'))
+
+
+    def list(self, request):
+        url = request.GET.get('url')
+        pdf = Pdf.objects.filter(redfin_src=url, deleted=False).first()
+
+        if not pdf:
+            raise NotFound(detail='PDF not found')
+
+        serializer = GetPdfSerializer(pdf)
+
+        return Response(serializer.data)
 
     def destroy(self, request):
-        pass
+        url = request.GET.get('url')
+        pdf = Pdf.objects.filter(redfin_src=url, deleted=False).first()
+
+        if not pdf:
+            raise NotFound(detail='PDF not found')
+
+        pdf.deleted = True
+        pdf.save()
+
+        serializer = GetPdfSerializer(pdf)
+
+        return Response(serializer.data)
