93cf4be2954a3544e1ec43e935bddf2a825e4bbd
Tech-Dex <rominu.alexandru@yahoo.com>
2020-10-15 18:27:13 +0300

    Test a new way to donwload PDF

--- frontend/src/pages/ContactInfo.vue
+++ frontend/src/pages/ContactInfo.vue
@@ -159,12 +159,13 @@ export default {
             method: 'GET'
           })
             .then(responseGet => {
-              var fileURL = window.URL.createObjectURL(new Blob([response.data]))
-              var fileLink = document.createElement('a')
-              fileLink.href = fileURL
-              fileLink.setAttribute('download', responseGet.data.pdf_src.split('/').pop())
-              document.body.appendChild(fileLink)
-              fileLink.click()
+              // var fileURL = window.URL.createObjectURL(new Blob([response.data]))
+              // var fileLink = document.createElement('a')
+              // fileLink.href = fileURL
+              // fileLink.setAttribute('download', responseGet.data.pdf_src.split('/').pop())
+              // document.body.appendChild(fileLink)
+              // fileLink.click()
+              this.showFile(response.data, responseGet.data.pdf_src.split('/').pop())
               this.loading = false
               localStorage.pdfBody = null
               localStorage.persistentChoices = null
@@ -194,6 +195,30 @@ export default {
       }
       )
       this.isLoaded = true
+    },
+    showFile(blob, fileName){
+      // It is necessary to create a new blob object with mime-type explicitly set
+      // otherwise only Chrome works like it should
+      var newBlob = new Blob([blob], {type: "application/pdf"})
+
+      // IE doesn't allow using a blob object directly as link href
+      // instead it is necessary to use msSaveOrOpenBlob
+      if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveOrOpenBlob(newBlob);
+        return;
+      }
+
+      // For other browsers:
+      // Create a link pointing to the ObjectURL containing the blob.
+      const data = window.URL.createObjectURL(newBlob);
+      var link = document.createElement('a');
+      link.href = data;
+      link.download=fileName;
+      link.click();
+      setTimeout(function(){
+        // For Firefox it is necessary to delay revoking the ObjectURL
+        window.URL.revokeObjectURL(data);
+      }, 100);
     }
   }
 }
