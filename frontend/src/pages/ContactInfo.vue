dd6a0d3639d118e427772216ff4452cc165c6b16
Tech-Dex <rominu.alexandru@yahoo.com>
2020-10-22 21:04:31 +0300

    #41/Broker Info with some of changes from another branch -> TO resolve Conflict

--- frontend/src/pages/ContactInfo.vue
+++ frontend/src/pages/ContactInfo.vue
@@ -21,11 +21,11 @@
           >
             <TextInput :special-field="true" v-model="pdfBody.designated_agent" title="Designated Agent"
                        text-label=" "></TextInput>
-            <TextInput :special-field="true" v-model="pdfBody.agent_mls" title="Agent MLS" text-label=" "></TextInput>
-            <TextInput :special-field="true" v-model="pdfBody.agent_license" title="Agent License"
+            <TextInput :special-field="true" v-model="pdfBody.agent_mls" title="Agent MLS#" text-label=" "></TextInput>
+            <TextInput :special-field="true" v-model="pdfBody.agent_license" title="Agent License#"
                        text-label=" "></TextInput>
             <TextInput :special-field="true" v-model="pdfBody.brokerage" title="Brokerage" text-label=" "></TextInput>
-            <TextInput :special-field="true" v-model="pdfBody.brokerage_mls" title="Brokerage MLS"
+            <TextInput :special-field="true" v-model="pdfBody.brokerage_mls" title="Brokerage MLS#"
                        text-label=" "></TextInput>
             <TextInput :special-field="true" v-model="pdfBody.brokerage_license" title="Brokerage License"
                        text-label=" "></TextInput>
@@ -36,18 +36,16 @@
             <TextInput :special-field="true" v-model="pdfBody.agent_fax" title="Agent Fax" text-label=" "></TextInput>
             <TextInput :special-field="true" v-model="pdfBody.broker_email" title="Broker Email"
                        text-label=" "></TextInput>
-            <CheckboxInput v-model="saveForFutureUseBroker" :special-field="true"
-                           text-label="Save for future use"></CheckboxInput>
             <CheckboxInput v-model="saveForFutureUseBrokerProfile" :special-field="true"
                            text-label="Save this profile for future use"></CheckboxInput>
             <b-dropdown id="dropdown-grouped" text="Buyer Broker Profiles" class="m-2">
               <b-dropdown-group v-for="(item, index) in brokerProfiles" :key="index">
                 <b-dropdown-item-button @click="selectProfile(index, 'broker')"> Profile {{index + 1}} </b-dropdown-item-button>
                 <b-dropdown-text>Designated Agent: <b>{{item.designated_agent}}</b></b-dropdown-text>
-                <b-dropdown-text>Agent Mls: <b>{{item.agent_mls}}</b></b-dropdown-text>
-                <b-dropdown-text>Agent License: <b>{{item.agent_license}}</b></b-dropdown-text>
+                <b-dropdown-text>Agent Mls#: <b>{{item.agent_mls}}</b></b-dropdown-text>
+                <b-dropdown-text>Agent License#: <b>{{item.agent_license}}</b></b-dropdown-text>
                 <b-dropdown-text>Brokerage: <b>{{item.brokerage}}</b></b-dropdown-text>
-                <b-dropdown-text>Brokerage Mls: <b>{{item.brokerage_mls}}</b></b-dropdown-text>
+                <b-dropdown-text>Brokerage Mls#: <b>{{item.brokerage_mls}}</b></b-dropdown-text>
                 <b-dropdown-text>Brokerage License: <b>{{item.brokerage_license}}</b></b-dropdown-text>
                 <b-dropdown-text>Broker Address: <b>{{item.broker_address}}</b></b-dropdown-text>
                 <b-dropdown-text>Agent Phone: <b>{{item.agent_phone}}</b></b-dropdown-text>
@@ -76,8 +74,6 @@
                        text-label=" "></TextInput>
             <TextInput :special-field="true" v-model="pdfBody.attorney_email" title="Attorney Email"
                        text-label=" "></TextInput>
-            <CheckboxInput v-model="saveForFutureUseAttorney" :special-field="true"
-                           text-label="Save for future use"></CheckboxInput>
             <CheckboxInput v-model="saveForFutureUseAttorneyProfile" :special-field="true"
                            text-label="Save this profile for future use"></CheckboxInput>
             <b-dropdown id="dropdown-grouped" text="Buyer Attorney Profiles" class="m-2">
@@ -112,8 +108,6 @@
             <TextInput :special-field="true" v-model="pdfBody.lender_fax" title="Lender Fax" text-label=" "></TextInput>
             <TextInput :special-field="true" v-model="pdfBody.lender_email" title="Lender Email"
                        text-label=" "></TextInput>
-            <CheckboxInput v-model="saveForFutureUseLender" :special-field="true"
-                           text-label="Save for future use"></CheckboxInput>
             <CheckboxInput v-model="saveForFutureUseLenderProfile" :special-field="true"
                            text-label="Save this profile for future use"></CheckboxInput>
             <b-dropdown id="dropdown-grouped" text="Buyer Lender Profiles" class="m-2">
@@ -171,9 +165,6 @@ export default {
       loading: false,
       pdfBody: new PdfBody(),
       persistentChoices: new PersistentChoices(),
-      saveForFutureUseBroker: false,
-      saveForFutureUseAttorney: false,
-      saveForFutureUseLender: false,
       saveForFutureUseBrokerProfile: false,
       saveForFutureUseAttorneyProfile: false,
       saveForFutureUseLenderProfile: false,
@@ -271,15 +262,6 @@ export default {
                 }
               }
               )
-              if (this.saveForFutureUseBroker) {
-                this.putDataOnLocalStorage(newPersistentChoices, new PersistentChoicesContactBroker())
-              }
-              if (this.saveForFutureUseAttorney) {
-                this.putDataOnLocalStorage(newPersistentChoices, new PersistentChoicesContactAttorney())
-              }
-              if (this.saveForFutureUseLender) {
-                this.putDataOnLocalStorage(newPersistentChoices, new PersistentChoicesContactAttorney())
-              }
               if (this.saveForFutureUseBrokerProfile) {
                 localStorage.brokerProfiles = JSON.stringify(this.createProfileForSectionInLocalStorage(this.brokerProfiles, new PersistentChoicesContactBroker()))
               }
@@ -336,14 +318,6 @@ export default {
           this.isLoaded = true
         }
       })
-    },
-    putDataOnLocalStorage (newPersistentChoices, type) {
-      Object.keys(new PersistentChoices()).forEach(key => {
-        if (key in type) {
-          newPersistentChoices[key] = this.pdfBody[key]
-        }
-      }
-      )
     },
     createProfileForSectionInLocalStorage (storage, type) {
       let item = type
