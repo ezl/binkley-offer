fb9170cf30bdc657acf14991e93e42739f37631a
Tech-Dex <rominu.alexandru@yahoo.com>
2021-03-02 17:12:42 +0200

    :fire: #122/Remove flask version

--- flask_backend/controllers/routes.py
+++
@@ -1,8 +0,0 @@
-from .pdf import PdfsApi, PdfApi
-from .search import SearchApi
-
-
-def initialize_routes(api):
-    api.add_resource(PdfsApi, '/api/pdfs')
-    api.add_resource(PdfApi, '/api/pdf')
-    api.add_resource(SearchApi, '/api/search')
