0f6a653ac702895a6d63331682efdd3f38e0e085
Tech-Dex <rominu.alexandru@yahoo.com>
2021-03-04 18:21:20 +0200

    :boom: Tweak SMTP service for Postmark

--- backend/pdf/services/email_service.py
+++ backend/pdf/services/email_service.py
@@ -1,6 +1,8 @@
 import smtplib
+import os
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from email.utils import formatdate
 
 from rest_framework.exceptions import NotFound
@@ -8,10 +10,10 @@ from rest_framework.exceptions import NotFound
 from pdf.models import Pdf
 from pdf.serializers import GetPdfSerializer
 
-EMAIL_SENDER = "dummy@email.com"
-EMAIL_SENDER_PASSWORD = 'dummy_password'
-SMTP_SERVER_HOST = "smtp.gmail.com"
-SMTP_SERVER_PORT = 587
+POSTMARK_SENDER_SIGNATURE = os.getenv("POSTMARK_SENDER_SIGNATURE")
+POSTMARK_SMTP_SERVER_HOST = os.getenv("POSTMARK_SMTP_SERVER_HOST")
+POSTMARK_SMTP_SERVER_PORT = int(os.getenv("POSTMARK_SMTP_SERVER_PORT"))
+POSTMARK_API_KEY = os.getenv("POSTMARK_API_KEY")
 
 
 def send_email(send_to, subject, pdf_id):
@@ -22,7 +24,7 @@ def send_email(send_to, subject, pdf_id):
 
     # Build message
     message = MIMEMultipart()
-    message["From"] = EMAIL_SENDER
+    message["From"] = POSTMARK_SENDER_SIGNATURE
     message["To"] = send_to
     message["Date"] = formatdate(localtime=True)
     message['Subject'] = subject
@@ -30,13 +32,14 @@ def send_email(send_to, subject, pdf_id):
         attach = MIMEApplication(f.read(), _subtype="pdf")
     attach.add_header('Content-Disposition', 'attachment', filename=str(serializer.data['pdf_src']))
     message.attach(attach)
+    message.attach(MIMEText("Body message <TO EDIT IT>"))
 
     # Build smtp client
-    smtp_client = smtplib.SMTP(SMTP_SERVER_HOST, SMTP_SERVER_PORT)
-    smtp_client.connect(SMTP_SERVER_HOST, SMTP_SERVER_PORT)
+    smtp_client = smtplib.SMTP(POSTMARK_SMTP_SERVER_HOST, POSTMARK_SMTP_SERVER_PORT)
+    smtp_client.connect(POSTMARK_SMTP_SERVER_HOST, POSTMARK_SMTP_SERVER_PORT)
     smtp_client.ehlo()
     smtp_client.starttls()
     smtp_client.ehlo()
-    smtp_client.login(EMAIL_SENDER, EMAIL_SENDER_PASSWORD)
-    smtp_client.sendmail(EMAIL_SENDER, send_to, message.as_string())
+    smtp_client.login(POSTMARK_API_KEY, POSTMARK_API_KEY)
+    smtp_client.sendmail(POSTMARK_SENDER_SIGNATURE, send_to, message.as_string())
     smtp_client.close()
